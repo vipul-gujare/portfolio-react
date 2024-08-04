8eb6459d56a4ffc9a53885ddda37cc3eb5b10da5
Vipul Gujare <vipulgujare@gmail.com>
2024-08-04 19:48:49 -0400

    Fix menu style

--- src/components/hamburger-menu/HamburgerMenu.tsx
+++ src/components/hamburger-menu/HamburgerMenu.tsx
@@ -17,12 +17,17 @@ export const HamburgerMenu = () => {
           alignItems: "center",
           position: "static",
           height: "100vh",
-          width: "1rem",
           border: `1px solid ${Colors.BORDER_LIGHT}`,
-          padding: "0.5rem",
         }}
       >
-        <ExplorerIcon fill={Colors.TEXT_LIGHT_ACTIVE} />
+        <div
+          style={{
+            borderLeft: `2px solid ${Colors.ACCENT}`,
+            padding: "0.5rem",
+          }}
+        >
+          <ExplorerIcon fill={Colors.TEXT_LIGHT_ACTIVE} />
+        </div>
       </div>
       <div
         style={{
