a78a649f171d4c1db4e2b69a0c54f19d6d1f4acb
Vipul Gujare <vipulgujare@gmail.com>
2024-08-05 10:56:00 -0400

    Add basic editable container

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,3 +1,24 @@
+import { Colors } from "../constants";
+
 export const MainContainer = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        backgroundColor: Colors.BACKGROUND_LIGHT,
+        flex: 1,
+      }}
+    >
+      <pre
+        contentEditable={true}
+        spellCheck={false}
+        className="main-container-editable"
+        suppressContentEditableWarning
+        style={{
+          width: "100%",
+        }}
+      >
+        Lorem Ipsum
+      </pre>
+    </div>
+  );
 };
